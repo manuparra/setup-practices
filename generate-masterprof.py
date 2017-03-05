8dd86181707246a1a174c03a073774c6ae5cf717
Manuel Parra <manuparra@gmail.com>
2017-03-05 13:47:40 +0100

    Added Public Key

--- generate-masterprof.py
+++ generate-masterprof.py
@@ -9,11 +9,15 @@ startinip=sys.argv[5]
 
 
 pupilslistdata=[]
+
 with open(pupilsfile) as f:
     pupilslist = f.read().splitlines() 
 
 for pup in pupilslist:
-    pupilslistdata.append({'username':pup.split("\t")[0],'name':pup.split("\t")[1]})
+	username=pup.split("\t")[0]
+	name=pup.split("\t")[1]
+	pupilslistdata.append({'username':username,'name':name})
+
 
 print "# GRANTED USER SSH"
 print "# ----------------"
@@ -40,21 +44,24 @@ for pupil in pupilslistdata:
 	print "echo '' | ssh-keygen -t rsa -f /home/" + account_name + "/.ssh/id_rsa "
 	print "exit"
 	print "oneuser create "+ account_name +" --ssh --key /home/"+ account_name +"/.ssh/id_rsa"
-	print "oneuser login "+ account_name +" --ssh --force"
+	print "cat /home/" + account_name + "/.ssh/id_rsa.pub"
+	print "Copy id_rsa.pub to opennebula user Public SSH Key"
 
-	"""
-	f=open ("/home/"+account_name + "/vnet.opennebula","w")
-	f.write("NAME = \""+account_name+"_vnet\"")
-	f.write("BRIDGE = br0")
-	f.write("DNS=150.214.191.10")
-	f.write("GATEWAY=192.168.10.1")
-	f.write("AR = [")
-    f.write("TYPE = IP4,")
-    f.write("IP = 192.168.10."+ip+",")
-    f.write("SIZE = 2")
-	f.write("]")
-	f.close()	
-	"""
+	
+
+	
+	#f=open ("/tmp/"+account_name + "vnet.opennebula","w")
+	#f.write("NAME = \""+account_name+"_vnet\"\n")
+	#f.write("BRIDGE = br0\n")
+	#f.write("DNS=150.214.191.10\n")
+	#f.write("GATEWAY=192.168.10.1\n")
+	#f.write("AR = [\n")
+	#f.write("TYPE = IP4,\n")
+	#f.write("IP = 192.168.10."+str(startinip)+",\n")
+	#f.write("SIZE = 2\n")
+	#f.write("]\n")
+	#f.close()	
+	
 	print startinip
 
 	startinip= int(startinip) + 2
